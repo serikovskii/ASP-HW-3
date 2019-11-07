6a3e84df16e65e07d0726be2c559ae086cdcfc18
Azat <46015824+serikovskii@users.noreply.github.com>
2019-11-07 13:41:12 +0600

    done

---
+++ WebApplication4/WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DownloadMusic(int music)
+        {
+            switch (music)
+            {
+                case 1: return File("~/music/deep.mp3", "audio/mpeg", "deep.mp3"); 
+                case 2: return File("~/music/lounge.mp3", "audio/mpeg", "lounge.mp3"); 
+                case 3: return File("~/music/club.mp3", "audio/mpeg", "club.mp3"); 
+                case 4: return File("~/music/cover.mp3", "audio/mpeg", "cover.mp3");
+                default: return HttpNotFound();
+            }
+            
+        }
+    }
+}
